ca3deeda62e2f298a8cd228150a952bf0300a2a1
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-03-07 20:36:05 +0000

    Actuallización para Autograding

--- miPrincipal/AppBusquedaBinaria.java
+++ miPrincipal/AppBusquedaBinaria.java
@@ -4,49 +4,17 @@ public class AppBusquedaBinaria{
         System.out.println("******************************");
         System.out.println("***.   Búsqueda Binaria.   ***");
         System.out.println("******************************");
-        int ele_bus = 45;
-        int arreglo[]={10,20,25,30,35,40,45,50};
-        System.out.println("Busqueda Binario Iterativa");
-        System.out.println("Posición del elemento: " +busquedaBinariaIte(arreglo,ele_bus));
-        System.out.println("Busqueda Binario Recursiva");
-        System.out.println("Posición del elemento: " +busquedaBinariaRec(arreglo,ele_bus,0,arreglo.length-1));
-        ele_bus = 51;
-        System.out.println("Posición del elemento: " +busquedaBinariaRec(arreglo,ele_bus,0,arreglo.length-1));
-        ele_bus = 1;
-        System.out.println("Posición del elemento: " +busquedaBinariaRec(arreglo,ele_bus,0,arreglo.length-1));
+       //coloque aquí el código faltante
     }
-    public static int busquedaBinariaIte(int []a, int ele){
-        int li=0;
-        int ld=a.length-1;
-       while (ld>=li)
-        {
-            int pos = (li+ld)/2;
-            if(a[pos]==ele)
-              return pos;
-            else if(ele>a[pos])
-               li=pos+1;
-            else
-               ld=pos-1;
-
-        }
-        return -1;
+    //coloque aquí el código faltabte
+    public static int busquedaBinariaIte(){
+       
 
     }
+    //coloque aquí el código faltante
 
-    public static int busquedaBinariaRec(int [] a,int ele, int li,int ld){
-        int pos = (li+ld)/2;
-        if (li>ld){
-           return -1;
-        }
-        else if (ele == a[pos]){
-            return pos;
-        }
-        else if (ele>a[pos]){
-            return busquedaBinariaRec(a,ele,pos+1,ld);
-        }
-        else{
-            return busquedaBinariaRec(a,ele,li,pos-1);
-        }
+    public static int busquedaBinariaRec(){
+        
 
 
 
